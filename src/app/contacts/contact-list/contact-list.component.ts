b027941306a15c33563594d1bc485df268f7946e
Zolzaya99 <zolzayaoyunkhand@gmail.com>
2024-01-27 12:05:23 -0500

    Week3 progress commit.

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Contact } from '../contact.model';
 
@@ -11,9 +11,14 @@ export class ContactListComponent implements OnInit {
   contacts: Contact[] = [
     new Contact('1', 'R. Kent Jackson','jacksonk@byui.edu','208-496-3771','../../assets/images/jacksonk.jpg', null),
     new Contact('2', 'Rex Barzee','barzeer@byui.edu','208-496-3768','../../assets/images/barzeer.jpg', null)
-
+    
   ];
+  
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
 
+  onSelected(contact:Contact) {
+    this.selectedContactEvent.emit(contact);
+  }
   constructor() { }
 
   ngOnInit() {
